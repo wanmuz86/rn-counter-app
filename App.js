cc93fae5acf28670f7cda1f0dd71500025dbf029
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2021-01-29 21:50:33 +0800

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  // Creating a variable counter, method setcounter, initialize to 0
+  const [counter, setCounter] = useState(0);
+  const [message, setMessage] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize:30,margin:15}}>{counter}</Text>
+      <Text>{message}</Text>
+
+      <Button title="Increment" color="green" onPress={() => {
+        setCounter(counter + 1);
+        setMessage('');
+
+      }} />
+      <Button title="Decrement" color="blue" onPress={() => {
+        if (counter > 0) {
+          setCounter(counter - 1)
+        }
+        else {
+          setMessage('Number cannot be less than 0');
+        }
+
+      }} />
+      <Button title="Reset" color="red" onPress={() => {
+        setCounter(0)
+        setMessage('');
+      }
+      } />
       <StatusBar style="auto" />
     </View>
   );
